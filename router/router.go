812c1c581430cf81badbb2b961039e69ea5429be
Jordan Santos <jordannegreirossantos@gmail.com>
2026-02-14 07:48:58 -0300

    feat: init router

---
+++ router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import "github.com/gin-gonic/gin"
+
+func Initialize() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := router.Run(":8080")
+
+	if err != nil {
+		panic(err)
+	}
+}
