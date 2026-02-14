b315fc4e129696879d8090f14ed64e137a1d2b5d
Jordan Santos <jordannegreirossantos@gmail.com>
2026-02-14 15:09:52 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "opportunities/router"
+import (
+	"opportunities/config"
+	"opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing config: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
