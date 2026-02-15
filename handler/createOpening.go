d345a945a971de392cad57748f0cf617ffd4b2eb
Jordan Santos <jordannegreirossantos@gmail.com>
2026-02-14 23:43:13 -0300

    feat: implement createOpening logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -2,12 +2,40 @@ package handler
 
 import (
 	"net/http"
+	"opportunities/schemas"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"msg": "POST openings",
-	})
+	request := CreateOpeningRequest{}
+
+	err := c.BindJSON(&request)
+	if err != nil {
+		logger.Errorf("binding request error: %s", err)
+		return
+	}
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err)
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Openings{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("create db error: %s", err)
+		sendError(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(c, "createOpening", opening)
 }
