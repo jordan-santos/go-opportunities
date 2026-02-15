571b7ee4a784c3e19f5aefcbb54e114c1a644db3
Jordan Santos <jordannegreirossantos@gmail.com>
2026-02-15 11:46:34 -0300

    feat: implement updateOpening logic

--- handler/request.go
+++ handler/request.go
@@ -44,3 +44,20 @@ func (req *CreateOpeningRequest) Validate() error {
 func errParamIsRequired(name, typ string) error {
 	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
 }
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (req *UpdateOpeningRequest) Validate() error {
+	if req.Role != "" || req.Company != "" || req.Location != "" || req.Remote != nil || req.Salary > 0 {
+		return nil
+	}
+
+	return fmt.Errorf("at least one param is required")
+}
--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -2,12 +2,67 @@ package handler
 
 import (
 	"net/http"
+	"opportunities/schemas"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpeningHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"msg": "PUT openings",
-	})
+	request := UpdateOpeningRequest{}
+
+	err := c.BindJSON(&request)
+	if err != nil {
+		logger.Errorf("UpdateOpeningHandler parse request body failed: %v", err)
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("UpdateOpeningHandler validate request failed: %v", err)
+		sendError(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := c.Query("id")
+	if id == "" {
+		sendError(c, http.StatusBadRequest, errParamIsRequired("id", "queryParam").Error())
+		return
+	}
+
+	opening := schemas.Openings{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(c, http.StatusBadRequest, "opening not found")
+		return
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errorf("UpdateOpeningHandler save opening failed: %v", err)
+		sendError(c, http.StatusInternalServerError, err.Error())
+	}
+
+	sendSuccess(c, "updateOpening", opening)
 }
